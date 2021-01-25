b577c77653f490fd26495660144d6a88359a786c
Fede <chiellofederico@gmail.com>
2021-01-25 15:17:15 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo. Poi aggiungete all'immagine una classe sempre presa dal data.
+
+var app = new Vue({
+  el: '#title',
+  data: {
+    message: 'Ciao a tutti dalla Norvegia',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAdX8rrmt9-u1L4Jzk61bj6d1fV_HvG1n_5A&usqp=CAU',
+    paesaggio: 'montagna'
+  }
+});
